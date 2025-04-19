b3f072a985d9a1ca9b32f4e52d55e40cf0167398
Oleksii Vagin <oleksii@leanconvert.com>
2025-04-19 12:13:36 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ChordWheel from './components/ChordWheel';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="-visually-hidden">Circle of fifths interactive SVG chord wheel (React version)</h1>
+      <p className="-visually-hidden">Interactive wheel showing chord relationships in the circle of fifths.</p>
+      <ChordWheel />
+    </div>
+  );
+}
+
+export default App;
